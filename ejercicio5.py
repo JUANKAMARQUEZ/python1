8ce81f7352d74bcfe1598cb09ad5955f5fee2d7c
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2024-10-21 17:57:49 +0200

    cambio ejercicio 5

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,3 +1,20 @@
 #5. Escribe un programa que genere una multiplicación de dos números del 2 al 10 al azar,
 # # pregunte por el resultado y diga si se ha dado la respuesta correcta  o no es correcta,
 # # y en este caso escribir la correcta.
+
+#se importa random
+import random
+
+#se crea los dos números aleatorios con random.Con randint conseguimos que los números sean enteros.
+numero1 = random.randint(2,10)
+numero2 = random.randint(2,10)
+resultado = numero1*numero2
+print(f"¿Cuanto es {numero1} x {numero2} ?")
+
+resultadoUsuario=int(input())
+
+if resultadoUsuario==resultado:
+    print(f"Es correcto.El resultado es {resultado}")
+else:
+    print(f"No es correcto. El resultado correcto es {resultado}")
+
