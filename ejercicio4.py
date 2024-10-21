360c14cb6fe381583db610580e06ed09a602e273
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2024-10-21 17:04:16 +0200

    creado ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,4 @@
+#4. Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice
+#  de masa corporal y lo almacene en una variable, y muestre por pantalla la frase 
+
+peso=int(input("Introduce tu peso en kg\n"))#hay que realizar un casting
