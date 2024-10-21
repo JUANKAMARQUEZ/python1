0d7a83444398542e022411bcdbb9cc39e397030d
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2024-10-21 16:36:31 +0200

    cambio

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,6 @@
 #3. Escribe los números pares del 2 hasta un número que se pida por teclado
 #previamente.
 
-numero=int(input("Introduce un número\n"))#hay que realizar un casting
+numero=int(input("Introduce un número\n"))#hay que realizar un castiqng
 for x in range(2, numero+2, 2):
   print(x) 
