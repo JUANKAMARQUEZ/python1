e7fc55dfa15229d85857c4bc616db857d63d2b92
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2024-10-21 17:32:14 +0200

    creado ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,3 @@
+#5. Escribe un programa que genere una multiplicación de dos números del 2 al 10 al azar,
+# # pregunte por el resultado y diga si se ha dado la respuesta correcta  o no es correcta,
+# # y en este caso escribir la correcta.
