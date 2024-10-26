b85b4bedfa155f4eb057e170f97bc593e69cfe44
JUANKAMARQUEZ <jcmarquezromero@gmail.com>
2024-10-26 17:17:24 +0200

    cambios en ejercicios 1 y 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -6,7 +6,7 @@
 x=input("Introduce su nombre\n")
 print(f"¡Hola, {x.upper()}!"); #con .upper contamos el número de letras que contiene el nombre contando los espacios
 
-print(f"La longuitud del nombre es de {len(x)}")
+print(f"La longuitud del nombre es de {len(x)}")#con "len" determinamos la longitud
 
 #bucle que imprime tantas veces el nombre como letras contiene el nombre y en lineas distintas
 for y in range(len(x)):
